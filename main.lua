c86930308c2f5bb961aa9cbd46df8e92cf8cc33a
Astrid <sakurasylmere@gmail.com>
2024-03-14 18:52:56 -0400

    Command handler testing
    I'm almost finishing the command handler; it's getting better, and I'll keep improving it over time.

--- main.lua
+++ main.lua
@@ -9,45 +9,47 @@ local Constructors = Discordia_slash.util.tools()
 local parseEnv = require('Utils.parseEnv')
 local Config = parseEnv:read('config.env')
 
+local commands = {
+    ping = require('./commands/ping.lua')
+}
+
 -- First impressions
 
 local function loadCommands()
-    --// Command handler testing
-    local commands = {
-        require('Commands.ping')
-    }
-    --[[
-    for _, command in ipairs(commands) do
+
+    for _, command in pairs(commands) do
         local commandConfig = command.getConfigs()
         Client:createGlobalApplicationCommand(commandConfig)
     end
-    ]]
+    
+    print('Commands loaded')
+end
+
+-- We need to delete all the previous registered commands and register the new ones
+local function deletePreviousCommands()
+    local Commands = Client:getGlobalApplicationCommands()
+    for CommandId in pairs(Commands) do
+        Client:deleteGlobalApplicationCommand(CommandId)
+    end
+
+    print('Deleted all old commands.')
 end
 
 local function Logged()
     print('Connected as ' .. Client.user.username)
-    loadCommands()
 
---[[
-        local Commands = Client:getGlobalApplicationCommands()
-    for CommandId in pairs(Commands) do
-        Client:deleteGlobalApplicationCommand(CommandId)
-    end
-    
-]]
+    deletePreviousCommands()
+    loadCommands()
     Client:info('Ready!')
 end
 
---[[
-Client:on('slashCommand', function(interaction, command, args)
-    if command.name == 'name' then
-        interaction:reply('testing!')
-    end
-end)
-]]
+local function commandsCallback(interaction, command, args)
+    commands[command.name]:reply(interaction, args)
+end
 
 -- Client configs
 
+Client:on('slashCommand', commandsCallback)
 Client:on('ready', Logged)
 Client:run(Config.TOKEN)
 Client:setActivity('Learning!')
